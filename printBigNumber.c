71f247847a2c5486a3837ea6a67e0797d45565c9
Amuguna <junjang99@naver.com>
2018-06-07 21:22:20 +0900

    add printBigNumber.c

---
+++ printBigNumber.c
@@ -0,0 +1,20 @@
+void printBigNumber(struct number number)
+{
+    int i, k=0;
+    printf("\n결과값: ");
+    for(i=0;i<=4;i++)
+    {
+        if(number.number[i] != 0 && number.number[i] > 0)
+            printf("%d", number.number[i]);
+        if(number.number[i] != 0 && number.number[i] < 0)
+        {
+            if(k==0)
+            {
+                printf("%d", number.number[i]);
+                k++;
+            }
+            else printf("%d", number.number[i] * (-1));
+        }
+    }
+}
+}
